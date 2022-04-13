045826a787549f3d0f47346090d82ebaa0f9fd7f
rominalodolo <lodoloromina@gmail.com>
2022-04-13 16:39:58 +0200

    update

--- DecisionMaking/src/Main.java
+++ DecisionMaking/src/Main.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Please enter your age: ");
+        int age = reader.nextInt();
+        if(age>= 18){
+            System.out.println("You are an adult.");
+        }
+        System.out.println("The program is over.");
+    }
 }
