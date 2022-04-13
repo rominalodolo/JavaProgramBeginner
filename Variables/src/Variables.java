34d7f42fe8ba719361f1375bcc26ed013e919034
rominalodolo <lodoloromina@gmail.com>
2022-04-13 11:54:53 +0200

    update

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -2,5 +2,21 @@ public class Variables {
     public static void main(String[] args) {
         String name = "Romina";
         System.out.println(name);
+
+        // This is a comment
+        // Romina likes working with Java text
+        // text variable
+        String myText = "I like Java";
+        System.out.println(myText);
+        myText = "new Text";
+        System.out.println(myText);
+
+        int myInteger = 100;
+        System.out.println(myInteger);
+
+        double myDoubleNumber = 55.6;
+        System.out.println(myDoubleNumber);
+        float myFloatNumber = 66.6F;
+        System.out.println(myFloatNumber);
     }
 }
