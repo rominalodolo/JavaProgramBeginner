8dbcdf53c79dcc35acfa4956fa214bac3844bd67
rominalodolo <lodoloromina@gmail.com>
2022-04-13 12:55:57 +0200

    update

--- Variables/src/Exercise.java
+++ Variables/src/Exercise.java
@@ -1,2 +1,12 @@
 public class Exercise {
+
+    public static void main(String[] args) {
+        String Chips = "I love eating chips";
+        System.out.println(Chips);
+
+        double Value = 3.14;
+        System.out.println(Value);
+
+        boolean 
+    }
 }
