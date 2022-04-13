51bd95984e81d39f736fd7dcd1ee660627ca4a63
rominalodolo <lodoloromina@gmail.com>
2022-04-13 11:48:27 +0200

    update

--- MyFirstProject/src/MyFirstClass.java
+++ MyFirstProject/src/MyFirstClass.java
@@ -1,12 +1,22 @@
 public class MyFirstClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello my Lovelies!");
+        System.out.println("Hello my Lovelies!"); // String
         System.out.println("How has your day been?"); // Second instruction that I'm executing
 
         System.out.print("My name is ");
         System.out.print("Romina"); // prints on the same line
 
-        System.out.println();
+        System.out.println(); // sout shortcut
+
+        System.out.println(1); // int
+        System.out.println(999); // int
+        System.out.println(2.45); // Double
+        System.out.println(2.45F); // Float
+        System.out.println(999999999999L); // Long
+        System.out.println(true); // Boolean: true or false
+        System.out.println(false);
+        System.out.println('a'); // char
+        System.out.println('%'); // char
     }
 }
