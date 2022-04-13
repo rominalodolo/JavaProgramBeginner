219a3c8654e90240af6a0e21ca810c6542e7a083
rominalodolo <lodoloromina@gmail.com>
2022-04-13 15:19:29 +0200

    update

---
+++ UserInput/src/UserInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Please enter a text: ");
+        String insertedText = reader.nextLine();
+
+    }
+}
