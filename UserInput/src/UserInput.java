48edba0bc68035b35853c444faa11d72091d4dca
rominalodolo <lodoloromina@gmail.com>
2022-04-13 15:36:34 +0200

    update

--- UserInput/src/UserInput.java
+++ UserInput/src/UserInput.java
@@ -4,17 +4,17 @@ public class UserInput {
     public static void main(String[] args) {
 
         Scanner reader = new Scanner(System.in);
-//        System.out.println("Please enter a text: ");
-//        String insertedText = reader.nextLine();
-//        System.out.println("You entered text: " + insertedText);
-//
-//        System.out.println("Please enter a number: ");
-//        int insertedNumber = reader.nextInt();
-//        System.out.println("You have entered the number " + insertedNumber);
+        System.out.println("Please enter a text: ");
+        String insertedText = reader.nextLine();
+        System.out.println("You entered text: " + insertedText);
 
-//        System.out.println(" Please enter a double number, with a comma instead of a dot: ");
-//        double insertedDouble = reader.nextDouble();
-//        System.out.println("You have entered a double number: " + insertedDouble); // use a comma
+        System.out.println("Please enter a number: ");
+        int insertedNumber = reader.nextInt();
+        System.out.println("You have entered the number " + insertedNumber);
+
+        System.out.println(" Please enter a double number, with a comma instead of a dot: ");
+        double insertedDouble = reader.nextDouble();
+        System.out.println("You have entered a double number: " + insertedDouble); // use a comma
 
 
         System.out.println("Please enter a float number(please use a comma for this system): ");
@@ -29,6 +29,8 @@ public class UserInput {
         boolean myBoolean = reader.nextBoolean();
         System.out.println("You have entered a boolean " + myBoolean);
 
-
+        System.out.println("Please Enter a character value: ");
+        char insertedChar = reader.next().charAt(0);
+        System.out.println("Provided char: " + insertedChar);
     }
 }
