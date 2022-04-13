55fd4e61a549ef0e32196a33568bf48473719f51
rominalodolo <lodoloromina@gmail.com>
2022-04-13 13:10:51 +0200

    update

--- Variables/src/Exercise.java
+++ Variables/src/Exercise.java
@@ -4,9 +4,24 @@ public class Exercise {
         String Chips = "I love eating chips";
         System.out.println(Chips);
 
+        char $and = '&';
+        System.out.println($and);
+
         double Value = 3.14;
         System.out.println(Value);
 
-        boolean 
+        boolean Value2 = true;
+        System.out.println(Value2);
+
+        System.out.print(Chips + $and + Value + Value2);
+
+        int num1 = 10;
+        int num2 = 6;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 * num2);
+
+
     }
 }
