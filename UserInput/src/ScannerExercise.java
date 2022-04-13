9628f454f0ca9fc1c8e712f156c3514f1c142786
rominalodolo <lodoloromina@gmail.com>
2022-04-13 16:27:53 +0200

    update

--- UserInput/src/ScannerExercise.java
+++ UserInput/src/ScannerExercise.java
@@ -17,6 +17,27 @@ public class ScannerExercise {
         System.out.printf("Oh I see, you're %d years old.", age);
 
         // 3.
+        Scanner reader1 = new Scanner(System.in);
+        System.out.println("Please enter your sex (M or F): ");
+        char gender = reader1.next().charAt(0);
+        System.out.println("You picked " + gender);
+        System.out.printf("You picked %c", gender);
+
+        // 4.
+        Scanner reader2 = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int n1 = reader2.nextInt();
+        System.out.println("Enter the second number: ");
+        int n2 = reader2.nextInt();
+        System.out.println("Enter the third number: ");
+        int n3 = reader2.nextInt();
+
+        int sum = n1 + n2 + n3;
+        int diff = n1 = n2 - n3;
+        int product = n1 * n2 * n3;
+        double average = sum / 3;
+
+        System.out.printf("The sum is equal to %d \n The difference is equal to %d \n" + " The product is equal to %d \n The average is equal to %f", sum, diff, product, average);
 
 
     }
