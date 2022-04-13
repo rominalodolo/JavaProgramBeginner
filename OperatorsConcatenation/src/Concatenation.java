4a19f9e9c8286d98adc5fb3d77bca1be0b81a8a2
rominalodolo <lodoloromina@gmail.com>
2022-04-13 14:55:59 +0200

    update

---
+++ OperatorsConcatenation/src/Concatenation.java
@@ -0,0 +1,19 @@
+public class Concatenation {
+    public static void main(String[] args) {
+        int number = 10;
+        String text = "test";
+        double doubleNum = 11.1;
+        float floatNum = 11.1F;
+        boolean truth = true;
+        char c = '*';
+        System.out.println("My string variable has the value " + text);
+        System.out.println("My number is " + number);
+        System.out.println("My Double number is " + doubleNum);
+        System.out.println("My Float number is " + floatNum);
+        System.out.println("I have declared a character with the value " + c);
+        System.out.println("Boolean is " + truth);
+        System.out.printf("I have declared a string variable with the value %s", text);
+
+
+    }
+}
--- OperatorsConcatenation/src/Operators.java
+++ OperatorsConcatenation/src/Operators.java
@@ -1,2 +1,46 @@
 public class Operators {
+    public static void main(String[] args) {
+
+        // Mathematical operators
+        // + - * / %
+
+        System.out.println(10 + 10);
+        System.out.println(100 - 87);
+        System.out.println(100 * 7);
+        System.out.println(40 / 2);
+        System.out.println(8 % 2);
+
+        // Relational operators
+
+        System.out.println( 7 == 9);
+        System.out.println( 7 != 9);
+        System.out.println(7 == 7 );
+        System.out.println(4 + 4 < 9);
+        System.out.println(8 <= 8);
+        System.out.println(8 >= 8);
+
+        // Logical operators
+        System.out.println(7 <10 && 100 <10);
+        System.out.println(100 < 1000 || 9 > 100);
+        System.out.println(! (2 >10));
+
+        // Assignment operators
+        int number = 5;
+        number++;
+        number++;
+        number++;
+        System.out.println(number);
+
+        int number2 = 8;
+        number2 --;
+        System.out.println(number2);
+
+        int number3 = 10;
+        number3 += 20;
+        System.out.println(number3);
+        number3 +=20;
+        System.out.println(number3);
+
+
+    }
 }
