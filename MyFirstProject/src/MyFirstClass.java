d864b07202cf3f0eef2c44fba9b817452d202707
rominalodolo <lodoloromina@gmail.com>
2022-04-13 11:00:07 +0200

    new

---
+++ MyFirstProject/src/MyFirstClass.java
@@ -0,0 +1,10 @@
+public class MyFirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello my Lovelies!");
+        System.out.println("How has your day been?"); // Second instruction that I'm executing
+
+        System.out.print("My name is ");
+        System.out.print("Romina");
+    }
+}
